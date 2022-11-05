0bdfeff406bb4022ff59642020cba4488988b544
Caitlyn Deslauriers <cdeslauriers2@nait.ca>
2022-11-05 16:51:40 -0600

    Getting rid of not working class

--- GameTest/Program.cs
+++ GameTest/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +11,49 @@ namespace GameTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
-            Console.ReadLine();
+            
+            
+
+
+
+
+
         }
+
+
+
+        static string getName()
+        {
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
